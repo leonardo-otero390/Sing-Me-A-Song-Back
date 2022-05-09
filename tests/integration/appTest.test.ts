0df65f05245b5833c06a17fc5827cab02d1fcbea
Leonardo Otero <leonardo.otero390@gmail.com>
2022-05-09 01:46:57 -0400

    feat: test upvote

--- tests/integration/appTest.test.ts
+++ tests/integration/appTest.test.ts
@@ -22,8 +22,12 @@ describe('POST /recommendations', () => {
 
 describe('POST /recommendations/:id/upvote', () => {
   it('should return 201', async () => {
-    const song = songFactory();
-    const response = await agent.post('/recommendations').send(song);
-    expect(response.status).toBe(201);
+    await utilsDatabase.populateDatabase();
+
+    const song = await utilsDatabase.getRandomRecommendation();
+    const response = await agent.post(`/recommendations/${song.id}/upvote`);
+    expect(response.status).toBe(200);
+    const updatedSong = await utilsDatabase.getRecommendationById(song.id);
+    expect(updatedSong.score).toBe(song.score + 1);
   });
 });
